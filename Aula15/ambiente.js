a72e9012671880d74be38d6dca857ee2027a1e91
Hércules Greogory Pereira de Souza <HG-Click@users.noreply.github.com>
2023-06-02 22:09:05 -0300

    Inico Aula 15 - JavaScript

---
+++ Aula15/ambiente.js
@@ -0,0 +1,23 @@
+// VARIAVEIS COMPOSTAS //
+
+// - Diferença entre variaveis simples e compostas
+    //VARIAVEIS SIMPLES    -   Só conseguem armazenar *UM VALOR* por vez
+    //VARIAVEIS COMPOSTAS  -   Devem ser capazes de armazenar *VÁRIOS VALORES* em uma mesma estrutua
+
+let vaga = [    ,   ,   ] //Ultilizar conchetes e virgula para cada chave ou índice
+// 1 - A "vaga" torna-se um VETOR ou ARRAY
+    //VETOR:Váriavel com vários espaços
+// 2 - Um vetor é composto de ELEMENTOS
+    //ELEMENTOS: Um par que agrupa o espaço da memória, o valor colocado dentro dele e o ÍNDICE
+//3 - Cada ELEMENTO é composto por ÍNDICE e seu VALOR
+    //ÍNDICE: tambem chamado de CHAVE (Key em inglês), trata-se do código de indentificão
+    //VALOR: O conteúdo da variavel
+
+//PRÁTICA
+let num = [5, 4, 3, 9, 8]
+num[3] = 6
+num.push(7)
+num.length
+num.sort()
+
+
