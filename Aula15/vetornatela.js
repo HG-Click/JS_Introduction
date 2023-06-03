0ebd6d49963d0aa20797fb93fb7967b9cbbe7655
Hércules Greogory Pereira de Souza <HG-Click@users.noreply.github.com>
2023-06-02 23:40:46 -0300

    Variaveis compostas - Aula15 de JS

--- Aula15/vetornatela.js
+++ Aula15/vetornatela.js
@@ -1,8 +1,10 @@
 //Usando a estrutura de repetição para as posições
-let valores = [8,1,7,8,5,4,2,3,6,5]
+let valores = [8,1,7,8,5]
 
-//console.log(valores)
+console.log(valores)
+
 /*
+//CONTAGEM MANUAL
 console.log(valores[0])
 console.log(valores[1])
 console.log(valores[2])
@@ -14,8 +16,17 @@ console.log(valores[7])
 console.log(valores[8])
 console.log(valores[9])
 console.log(valores[10])
+
+
+//CONTAGEM COM AUTOMAÇÃO SIMPLES
+console.log(`Considerando o VETOR ${valores}`)
+
+for(let pos=0;pos < valores.length;pos++){
+    console.log(`A posição ${pos} deste Vetor é igual a ${valores[pos]}`)
+}
 */
 
-for(let pos=0;pos <= valores.length;pos++){
-    console.log(`A posição ${pos} deste VETOR é igual a ${valores(pos)}`)
+//CONTAGEM COM AUTOMAÇÃO APRIMORADA
+for(let pos in valores){ // -- Para cada posição(pos) dentro do VETOR (valores)
+    console.log(`A posição ${pos} deste VETOR é igual a ${valores[pos]}`) //Fazer  o seguinte processo
 }
