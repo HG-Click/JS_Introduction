fd5494cb65209828de0bf618af0e95348a16f364
Hércules Greogory Pereira de Souza <HG-Click@users.noreply.github.com>
2023-06-02 23:18:40 -0300

    Iniciando variaveis compostas

--- Aula15/ambiente.js
+++ Aula15/ambiente.js
@@ -1,23 +1,28 @@
 // VARIAVEIS COMPOSTAS //
-
-// - Diferença entre variaveis simples e compostas
-    //VARIAVEIS SIMPLES    -   Só conseguem armazenar *UM VALOR* por vez
-    //VARIAVEIS COMPOSTAS  -   Devem ser capazes de armazenar *VÁRIOS VALORES* em uma mesma estrutua
+/*
+Diferença entre variaveis simples e compostas
+    VARIAVEIS SIMPLES    -   Só conseguem armazenar *UM VALOR* por vez
+    VARIAVEIS COMPOSTAS  -   Devem ser capazes de armazenar *VÁRIOS VALORES* em uma mesma estrutua
 
 let vaga = [    ,   ,   ] //Ultilizar conchetes e virgula para cada chave ou índice
-// 1 - A "vaga" torna-se um VETOR ou ARRAY
-    //VETOR:Váriavel com vários espaços
-// 2 - Um vetor é composto de ELEMENTOS
-    //ELEMENTOS: Um par que agrupa o espaço da memória, o valor colocado dentro dele e o ÍNDICE
-//3 - Cada ELEMENTO é composto por ÍNDICE e seu VALOR
-    //ÍNDICE: tambem chamado de CHAVE (Key em inglês), trata-se do código de indentificão
-    //VALOR: O conteúdo da variavel
-
-//PRÁTICA
-let num = [5, 4, 3, 9, 8]
-num[3] = 6
-num.push(7)
-num.length
-num.sort()
+1 - A "vaga" torna-se um VETOR ou ARRAY
+    VETOR:Váriavel com vários espaços
+2 - Um vetor é composto de ELEMENTOS
+    ELEMENTOS: Um par que agrupa o espaço da memória, o valor colocado dentro dele e o ÍNDICE
+3 - Cada ELEMENTO é composto por ÍNDICE e seu VALOR
+    ÍNDICE: tambem chamado de CHAVE (Key em inglês), trata-se do código de indentificão (posição)
+    VALOR: O conteúdo da variavel
+*/
+//EXEMPLOS
+let num = [5, 4, 3] 
+console.log(num)
+num[3] = 6 //Coloca o valor 6 na posição 3 automaticamente SAIDA : [5, 4 ,3 , 6]
+console.log(`Com o valor ${num[3]}, o novo vetor é igual a ${num}`)
+num.push(7) //Cria uma nova posição e inclui o valor 7 SAIDA : [5, 4 ,3 , 6, 7]
+console.log(`Incluindo o novo valor ${num[4]} no ultimo elemento, o VETOR fica igual a ${num}`)
+num.length //Retorna o comprimento, ou quantidade de elementos do VETOR SAIDA: 5
+console.log(`A quantidade de elementos deste VETOR é igual a ${num.length} posições`)
+num.sort() //Deixa os elementos ordenados SAIDA: [3, 4, 5, 6, 7]
+console.log(`Colocando esse vetor ordenado fica ${num.sort()}`)
 
 
---
+++ Aula15/vetornatela.js
@@ -0,0 +1,21 @@
+//Usando a estrutura de repetição para as posições
+let valores = [8,1,7,8,5,4,2,3,6,5]
+
+//console.log(valores)
+/*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5])
+console.log(valores[6])
+console.log(valores[7])
+console.log(valores[8])
+console.log(valores[9])
+console.log(valores[10])
+*/
+
+for(let pos=0;pos <= valores.length;pos++){
+    console.log(`A posição ${pos} deste VETOR é igual a ${valores(pos)}`)
+}
